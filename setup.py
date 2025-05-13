a9885d3752d73e0af115caa64669bb0c75191ace
Mürsel Taşgın <TB19409@turkbank.com.tr>
2025-05-13 15:11:12 +0300

    dir structure changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="alphaseeker",
+    name="alpha_seeker",
     version="0.1.1",
     author="Mursel Tasgin",
     author_email="mursel.tasgin@gmail.com",
